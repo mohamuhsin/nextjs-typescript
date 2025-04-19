6e7cbd116504d99381671173f83f909687f47ae5
Moha Muhsin <mohamuhsin101@gmail.com>
2025-04-20 00:35:02 +0300

    created routes

---
+++ app/meals/[mealslug]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MealsDetailsPage = () => {
+  return <h1>Meals Details Page</h1>;
+};
+
+export default MealsDetailsPage;
---
+++ app/meals/share/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SharePage = () => {
+  return <h1>Share Page</h1>;
+};
+
+export default SharePage;
