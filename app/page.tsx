914ec2291346a19668fe67b78086829acaa6997c
Moha Muhsin <mohamuhsin101@gmail.com>
2025-04-20 00:03:03 +0300

    ready for the foodies app

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-export default function Home() {
+import React from "react";
+
+const Home = () => {
   return (
     <main>
-      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
-      <h1>Welcome to this NextJS Course!</h1>
-      <p>🔥 Let&apos;s get started! 🔥</p>
+      <h1 style={{ color: "white", textAlign: "center" }}>
+        Time to get started!
+      </h1>
     </main>
   );
-}
+};
+
+export default Home;
