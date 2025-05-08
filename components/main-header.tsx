3332008f0daf0baa92fb740d3f88478c288e015c
Moha Muhsin <mohamuhsin101@gmail.com>
2025-05-08 15:35:39 +0300

    optimizing images with nextjs

--- components/main-header.tsx
+++ components/main-header.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import Image from "next/image";
+
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 
@@ -6,7 +8,7 @@ const MainHeader = () => {
   return (
     <header className={classes.header}>
       <Link href="/" className={classes.logo}>
-        <img src={logoImg.src} alt="A plate with food on it" />
+        <Image src={logoImg} alt="A plate with food on it" priority />
         NextLevel Food
       </Link>
 
